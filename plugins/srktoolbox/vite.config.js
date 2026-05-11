0550018d3d4429ddfe76bc678557ec71eea083ee
LAOBILAXI <sb1344155394@163.com>
2026-05-11 16:42:42 +0800

    first

---
+++ plugins/srktoolbox/vite.config.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    plugins: [vue()],
+    base: "./"
+});
+
+=======
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './'
+})
+>>>>>>> 334f3fc (first commit)
